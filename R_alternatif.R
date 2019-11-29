482deb43173b525b4512837be14f55146d5e2de6
Unknown <marcandreditlebreton@hotmail.com>
2019-11-29 12:14:25 -0500

    regression polynomiale degree 3

--- R_alternatif.R
+++ R_alternatif.R
@@ -6,10 +6,22 @@ donnees$date<-as.Date(donnees$date)
 
 donnees$annee <- as.factor(year(donnees$date))
 
+ggplot(donnees, aes(x = temp, y = prod))+geom_point()+geom_smooth()
+
+
 donnees <- donnees[which(donnees$annee == 2018),]
 
 str(donnees)
 
-ggplot(donnees, aes(x = temp, y = prod, color = annee))+geom_point()+geom_smooth()
+ggplot(donnees, aes(x = temp, y = prod))+geom_point()+geom_smooth()
+
+model <- lm(donnees$prod ~ poly(donnees$temp,3))
+
+plot(model)
+
+summary(model)
 
 head(donnees,12)
+
+
+
