ded7c0f25d210689b1356a7aacd6caf7e7e0c2a5
Unknown <marcandreditlebreton@hotmail.com>
2019-12-01 10:37:24 -0500

    ajout de modeles loi normale pour prod et temp et regression pour tous

--- R_alternatif.R
+++ R_alternatif.R
@@ -2,26 +2,59 @@ library(lubridate)
 setwd(dir="C:\\Users\\marca\\Desktop\\MH2302D")
 
 
-donnees <- read.csv("BonnesDonnes.csv")
+donneesTot <- read.csv("BonnesDonnes.csv")
 
 #colnames(donnees) <- c('id','date','prod','temp','prix')
 
-donnees$date<-as.Date(donnees$date)
+donneesTot$date<-as.Date(donneesTot$date)
 
-donnees$annee <- as.factor(year(donnees$date))
+donneesTot$annee <- as.factor(year(donneesTot$date))
 
-ggplot(donnees, aes(x = temp, y = prod))+geom_point()+geom_smooth()
+#Preparation des donnees sous formes de Time series pour analyse de la cyclicique et de l'auto correlation
+tempTs <- ts(donneesTot$temp, start = c(2008, 1), end=c(2018, 2), frequency = 12)
+prodTs <- ts(donneesTot$prod, start = c(2008, 1), end=c(2018, 2), frequency = 12)
+prixTs <- ts(donneesTot$prix, start = c(2008, 1), end=c(2018, 2), frequency = 12)
 
-ggplot(donnees, aes(x = temp, y = prix))+geom_point()+geom_smooth()
-donnees <- donnees[which(donnees$annee == 2017 |donnees$annee == 2018),]
+#Selection des annees a observer
+donnees <- donneesTot[which(donneesTot$annee == 2016 |donneesTot$annee == 2017),]
 
-str(donnees)
 
-ggplot(donnees, aes(x = temp, y = prod))+geom_point()+geom_smooth()
 
-model <- lm(donnees$prod ~ poly(donnees$temp,3))
 
-plot(model)
+
+############################ Histogramme des variables #########################
+#Aka distribution des donnees
+
+#histogramme de la quantite d'electricite
+hist( donneesTot$prod,col="red",main="Histogramme de la production moyenne d'electricte",
+      xlab="Intervalle de megawatt/heure",ylab="Effectif", labels = T)
+
+#Histogramme du prix de l'electricite
+hist( donneesTot$prix,col="green",main="Histogramme du prix de vente moyen de l'electricite",
+      xlab="Intervalle de prix",ylab="Effectif", labels = T)
+
+#Histogramme de la temperature
+hist( donneesTot$temp,col="orange",main="Histogramme de la temperature moyenne",
+      xlab="Intervalle de temperature",ylab="Effectif", labels = T)
+
+
+
+
+
+
+#Illustration du cycle de la temperature et de la production d'electricite
+
+boxplot(tempTs~cycle(tempTs))
+
+boxplot(prodTs~cycle(prodTs))
+
+boxplot(prixTs~cycle(prixTs))
+
+
+
+
+
+##### Tests de normalite
 
 #Test normalite temperature
 shapiro.test(donnees$temp)
@@ -30,11 +63,48 @@ shapiro.test(donnees$prod)
 #test Normalite prix
 shapiro.test(donnees$prix)
 
-#En gros il ne semble rien y avoir de normalement distribue
 
+#variance et moyenne
+#temp
+var(donnees$temp)
+mean(donnees$temp)
+
+#prod
+var(donnees$prod)
+mean(donnees$prod)
+
+#prix
+var(donnees$prix)
+mean(donnees$prix)
+
+########Generation des relations et regression polynomiale
+
+# Temp et prod
+
+str(donnees)
+ggplot(donnees, aes(x = temp, y = prod))+geom_point()+geom_smooth()
+
+model <- lm(donnees$prod ~ poly(donnees$temp,3))
+
+plot(model)
 summary(model)
 
 head(donnees,12)
+
+# Prix et temp
+ggplot(donnees, aes(x = temp, y = prix))+geom_point()+geom_smooth()
+# Ne donne rien
+
+#Polynomiale
+modelTempPrix <- lm(donnees$prix ~ poly(donnees$temp,3))
+summary(modelTempPrix)
+
+#On voit ici que ca marche pas tant
+
+#Generation d'un modele time series pour temp et prod :
+
+
+
 
 
 
